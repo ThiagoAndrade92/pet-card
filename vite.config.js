f97a6f235ea016aaa9d937230e37d30fc676889c
ThiagoAndrade92 <tihaguinho92@gmail.com>
2025-05-16 21:21:29 -0300

    corrigindo erro de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://github.com/ThiagoAndrade92/pet-card'
+  base:'/pet-card'
 })
